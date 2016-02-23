e7ed7da038bd864902e31a18b40be9bb2260e22e
Main Khan <mukhan@umass.edu>
2016-02-22 20:17:41 -0500

    added some real sound clips, answers functionality finished

--- KanyeBoard/ViewController.m
+++ KanyeBoard/ViewController.m
@@ -10,7 +10,11 @@
 
 @interface ViewController ()
 {
-    AVAudioPlayer *_answersPlayer;
+    AVAudioPlayer *_answers1Player;
+    AVAudioPlayer *_answers2Player;
+    AVAudioPlayer *_answers3Player;
+    AVAudioPlayer *_answers4Player;
+    int _answersCount;
     
 }
 
@@ -19,20 +23,68 @@
 @implementation ViewController
 
 - (IBAction)answersPush:(UIButton *)sender {
-    [_answersPlayer play];
+    // Cycle between 1 of 4 answers sounds
+    if(_answersCount >=4 || _answersCount == 0){
+        _answersCount=1;
+    }
+    else{
+        NSLog(@"Got here count=%d",_answersCount);
+        _answersCount=_answersCount+1;
+    }
+    
+    switch (_answersCount) {
+        case 1:
+            [_answers1Player play];
+            break;
+        case 2:
+            [_answers2Player play];
+            break;
+        case 3:
+            [_answers3Player play];
+            break;
+        case 4:
+            [_answers4Player play];
+            break;
+        default:
+            NSLog(@"Intefer out of range");
+            break;
+    }
+    
 }
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+   
+    // Initialize answers cycle counter
+    _answersCount =0;
     
     // Construct URL to sound file
-    NSString *answersPath = [NSString stringWithFormat:@"%@/cheer.mp3", [[NSBundle mainBundle] resourcePath]];
-    NSURL *answersUrl = [NSURL fileURLWithPath: answersPath];
+    NSString *soundPath = [NSString stringWithFormat:@"%@/answers1.mp3", [[NSBundle mainBundle] resourcePath]];
+    NSURL *soundUrl = [NSURL fileURLWithPath: soundPath];
     
     // Create audio player object and initialize with URL to sound
-    _answersPlayer = [[AVAudioPlayer alloc] initWithContentsOfURL:answersUrl error:nil];
+    _answers1Player = [[AVAudioPlayer alloc] initWithContentsOfURL:soundUrl error:nil];
     
+    // Construct URL to sound file
+    soundPath = [NSString stringWithFormat:@"%@/answers2.mp3", [[NSBundle mainBundle] resourcePath]];
+    soundUrl = [NSURL fileURLWithPath: soundPath];
+    
+    // Create audio player object and initialize with URL to sound
+    _answers2Player = [[AVAudioPlayer alloc] initWithContentsOfURL:soundUrl error:nil];
+    
+    // Construct URL to sound file
+    soundPath = [NSString stringWithFormat:@"%@/answers3.mp3", [[NSBundle mainBundle] resourcePath]];
+    soundUrl = [NSURL fileURLWithPath: soundPath];
+    
+    // Create audio player object and initialize with URL to sound
+    _answers3Player = [[AVAudioPlayer alloc] initWithContentsOfURL:soundUrl error:nil];
+    
+    // Construct URL to sound file
+    soundPath = [NSString stringWithFormat:@"%@/answers4.mp3", [[NSBundle mainBundle] resourcePath]];
+    soundUrl = [NSURL fileURLWithPath: soundPath];
+    
+    // Create audio player object and initialize with URL to sound
+    _answers4Player = [[AVAudioPlayer alloc] initWithContentsOfURL:soundUrl error:nil];
 }
 
 - (void)didReceiveMemoryWarning {
